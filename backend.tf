3c7ecf2211fa9691fe2172241c75242dc4ac4c91
More Pranjal <more.pranjalthakaram@hcl.com>
2024-11-29 13:23:04 +0530

    11

--- backend.tf
+++ backend.tf
@@ -8,5 +8,6 @@ terraform {
     client_secret   = "CP78Q~XddXub5dQM_hgRim.hrj8Oi2-HTnZepb~i"
     subscription_id          = "505743ae-49da-49ce-b0a4-eeb2cc0f8f0e"
     tenant_id                = "f77d0bdc-0d72-4b3d-8d8c-d87448c17ef8"
+    use_azuread_auth = true
   }
 }
