b1ac71709ea4b559e954fe3ed4d38c898bee38bc
More Pranjal <more.pranjalthakaram@hcl.com>
2024-11-29 11:57:13 +0530

    new4

--- provider.tf
+++ provider.tf
@@ -6,12 +6,14 @@ terraform {
     }
   }
 }
+variable "client_secret" {
+}
 
 provider "azurerm" {
   features {}
 
   client_id       = "45b5de6f-333e-4e0b-9146-441630f7a13a"
-  client_secret   = "CP78Q~XddXub5dQM_hgRim.hrj8Oi2-HTnZepb~i"
+  client_secret   = var.client_secret
   tenant_id       = "f77d0bdc-0d72-4b3d-8d8c-d87448c17ef8"
   subscription_id = "505743ae-49da-49ce-b0a4-eeb2cc0f8f0e"
 }
