9ef6b1cb39c38a422a9ce3a4a03d091a02b1ccb6
More Pranjal <more.pranjalthakaram@hcl.com>
2024-11-29 13:18:10 +0530

    new9

--- backend.tf
+++ backend.tf
@@ -1,8 +1,10 @@
+
 terraform {
   backend "azurerm" {
-      resource_group_name  = "TanuRG"
-      storage_account_name = "3novteststrg"
-      container_name       = "ios"
-      key                  = "terraform.tfstate"
+    storage_account_name     = "3novteststrg"
+    container_name           = "ios"
+    key                      = "terraform.tfstate"
+    subscription_id          = "505743ae-49da-49ce-b0a4-eeb2cc0f8f0e"
+    tenant_id                = "f77d0bdc-0d72-4b3d-8d8c-d87448c17ef8"
   }
 }
