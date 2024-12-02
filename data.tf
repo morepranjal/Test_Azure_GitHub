a7d6c84ad7c1944c1c4d8ba9fbd42fe032deaef5
More Pranjal <more.pranjalthakaram@hcl.com>
2024-12-02 15:28:43 +0530

    23

---
+++ data.tf
@@ -0,0 +1,19 @@
+data "azurerm_key_vault" "keyvault_devops" {
+ name                = "github-test"
+ resource_group_name = "TerraformGitHubRG2"
+}
+
+data "azurerm_key_vault_secrets" "global_devops" {
+ key_vault_id = data.azurerm_key_vault.keyvault_devops.id
+}
+
+data "azurerm_key_vault_secret" "global_devops" {
+ for_each     = toset(data.azurerm_key_vault_secrets.global_devops.names)
+ name         = each.key
+ key_vault_id = data.azurerm_key_vault.keyvault_devops.id
+}
+
+output "secret_value" {
+  value = data.azurerm_key_vault_secret.global_devops.name.value
+
+}
