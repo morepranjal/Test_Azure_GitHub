23bef514681bf164073aa08d622aca0fbefc14a2
More Pranjal <more.pranjalthakaram@hcl.com>
2024-12-03 10:35:45 +0530

    25

--- data.tf
+++ data.tf
@@ -1,19 +1,35 @@
-data "azurerm_key_vault" "keyvault_devops" {
+# data "azurerm_key_vault" "keyvault_devops" {
+#  name                = "github-test"
+#  resource_group_name = "TerraformGitHubRG2"
+# }
+
+# data "azurerm_key_vault_secrets" "global_devops" {
+#  key_vault_id = data.azurerm_key_vault.keyvault_devops.id
+# }
+
+# data "azurerm_key_vault_secret" "global_devops" {
+#  for_each     = toset(data.azurerm_key_vault_secrets.global_devops.names)
+#  name         = each.key
+#  key_vault_id = data.azurerm_key_vault.keyvault_devops.id
+# }
+
+# output "secret_value" {
+#   value = { for key, secret in data.azurerm_key_vault_secret.global_devops : key => secret.value }
+#   #sensitive = true
+# }
+
+
+
+data "azurerm_key_vault" "existing" {
  name                = "github-test"
  resource_group_name = "TerraformGitHubRG2"
 }
 
-data "azurerm_key_vault_secrets" "global_devops" {
- key_vault_id = data.azurerm_key_vault.keyvault_devops.id
-}
-
-data "azurerm_key_vault_secret" "global_devops" {
- for_each     = toset(data.azurerm_key_vault_secrets.global_devops.names)
- name         = each.key
- key_vault_id = data.azurerm_key_vault.keyvault_devops.id
+data "azurerm_key_vault_secret" "example" {
+  name         = "subscriptionid"
+  key_vault_id = data.azurerm_key_vault.existing.id
 }
 
 output "secret_value" {
-  value = { for key, secret in data.azurerm_key_vault_secret.global_devops : key => secret.value }
-  #sensitive = true
+  value = nonsensitive(data.azurerm_key_vault_secret.example.value)
 }
