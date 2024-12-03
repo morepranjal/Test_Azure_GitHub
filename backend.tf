e6cd09b70f01bfc1b136e5accb33b35994ddbb5c
More Pranjal <more.pranjalthakaram@hcl.com>
2024-12-03 16:32:31 +0530

    secrethide1

--- backend.tf
+++ backend.tf
@@ -6,7 +6,7 @@ terraform {
     resource_group_name      = "TanuRG"
     storage_account_name     = "3novteststrg"
     container_name           = "ios"
-    key                      = "terraform11.tfstate"
+    key                      = "terraform1234.tfstate"
 
   }
 }
--- storageaccount/stg.tf
+++ storageaccount/stg.tf
@@ -1,7 +1,7 @@
 
 
 resource "azurerm_storage_account" "example" {
-  name                     = "githubstgaccount56743"
+  name                     = "githubstgaccount5678643"
   resource_group_name      = var.rg_name
   location                 = var.deploy_location
   account_tier             = "Standard"
