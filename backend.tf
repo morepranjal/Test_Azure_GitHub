0f909a8399e22eac945a6cfe88d3fec85c422520
More Pranjal <more.pranjalthakaram@hcl.com>
2024-12-03 12:57:17 +0530

    30

--- backend.tf
+++ backend.tf
@@ -5,13 +5,29 @@ terraform {
     storage_account_name     = "3novteststrg"
     container_name           = "ios"
     key                      = "terraform.tfstate"
-    # client_id                = "45b5de6f-333e-4e0b-9146-441630f7a13a"
-    # client_secret            = "CP78Q~XddXub5dQM_hgRim.hrj8Oi2-HTnZepb~i"
-    # subscription_id          = "505743ae-49da-49ce-b0a4-eeb2cc0f8f0e"
-    # tenant_id                = "f77d0bdc-0d72-4b3d-8d8c-d87448c17ef8"
+    client_id                = var.client_id
+    client_secret            = var.client_secret
+    subscription_id          = var.subscription_id
+    tenant_id                = var.tenant_id
    # use_azuread_auth         = true
   }
 }
+
+
+
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name      = "TanuRG"
+#     storage_account_name     = "3novteststrg"
+#     container_name           = "ios"
+#     key                      = "terraform.tfstate"
+#     # client_id                = "45b5de6f-333e-4e0b-9146-441630f7a13a"
+#     # client_secret            = "CP78Q~XddXub5dQM_hgRim.hrj8Oi2-HTnZepb~i"
+#     # subscription_id          = "505743ae-49da-49ce-b0a4-eeb2cc0f8f0e"
+#     # tenant_id                = "f77d0bdc-0d72-4b3d-8d8c-d87448c17ef8"
+#    # use_azuread_auth         = true
+#   }
+# }
 
 
 
--- variable.tf
+++ variable.tf
@@ -3,3 +3,16 @@ variable "rg_name" {
   description = "Name of the Resource group in which to deploy shared resources"
   default     = "TerraformGitHubRG2"
 }
+
+variable "client_id" {
+  type = string 
+}
+variable "client_secret" {
+  type = string 
+}
+variable "subscription_id" {
+  type = string 
+}
+variable "tenant_id" {
+  type = string 
+}
